de676ad8394a87282bd5b1ad5ce2551e1c0f2427
linhdq <linhdqse03570@gmail.com>
2016-12-19 18:33:27 +0700

    add splash screen

--- app/src/main/java/com/example/linhdq/taxi/MainActivity.java
+++ app/src/main/java/com/example/linhdq/taxi/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //start animation
+        overridePendingTransition(R.anim.trans_in, R.anim.trans_out);
     }
 }
---
+++ app/src/main/java/com/example/linhdq/taxi/activities/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.linhdq.taxi.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.linhdq.taxi.MainActivity;
+import com.example.linhdq.taxi.R;
+
+/**
+ * Created by LinhDQ on 12/19/16.
+ */
+
+public class SplashScreenActivity extends AppCompatActivity {
+    //time out
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                //start activity
+                SplashScreenActivity.this.startActivity(intent);
+                SplashScreenActivity.this.finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
