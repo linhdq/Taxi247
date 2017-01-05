df22f53c9ff056eb078f7763c2a54ce27dab5867
linhdq <linhdqse03570@gmail.com>
2017-01-05 15:11:33 +0700

    com mít

--- app/src/main/java/com/example/linhdq/taxi/fragment/BookingFragment.java
+++ app/src/main/java/com/example/linhdq/taxi/fragment/BookingFragment.java
@@ -7,14 +7,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.linhdq.taxi.R;
 import com.example.linhdq.taxi.adapter.CarTypePagerAdapter;
